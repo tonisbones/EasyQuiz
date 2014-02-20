37bdef663acda1453da0e0f6a6ee59b5506fd283
tonisbones <decausem@gmail.com>
2014-02-20 15:18:21 -0500

    added render_template to import so it would actually work correctly

--- EasyQuiz.py
+++ EasyQuiz.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return render_template('index.html');
+def index():
+	return render_template('index.html')
 if __name__ == "__main__":
 	app.run()
